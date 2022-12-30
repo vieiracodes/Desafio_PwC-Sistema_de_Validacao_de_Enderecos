5a341d930ec0338d1857a55ad3daa5d129e67720
Gabriel Vieira <vieira.front-end@outlook.com>
2022-12-30 16:21:41 -0300

    Criação do componente TestCases

--- src/main/java/gabriel/io/components/AddressValidator.java
+++ src/main/java/gabriel/io/components/AddressValidator.java
@@ -1,4 +1,59 @@
 package gabriel.io.components;
 
+import java.util.Arrays;
+
 public class AddressValidator {
+
+    public static String[] validate(String originalAddress){
+
+        originalAddress = originalAddress.replaceAll("[#,]", "");
+
+        //Separa o endereço digitado em um array
+        String[] addressFragments = originalAddress.split(" ");
+
+        /*Variável que guardará o endereço formatado sendo que:
+            formattedAddress[0] = Guardará o endereço
+            formattedAddress[1] = Guardará o número
+         */
+        String[] formattedAddress = new String[2];
+        formattedAddress[0] = formattedAddress[1] = "";
+
+        /*Variável que guardará o número do endereço sendo que:
+            addressNum[0] = Guardará "No" (caso exista),
+            addressNum[1] = Guardará o número em si,
+            addressNum[2] = Guardará o bloco (caso exista)
+         */
+        String[] addressNum = new String[3];
+
+        for (int i = 0; i < addressFragments.length; i++) {
+            if(isInteger(addressFragments[i])){
+                addressNum[1] = addressFragments[i];
+                //Verifica se existe um carácter de bloco após o número
+                if (i + 1 < addressFragments.length && addressFragments[i+1].length() == 1 ){
+                    addressNum[2] = addressFragments[i+1];
+                }
+                //Verifica se existe o "No" antes do número
+                if ( i - 1 >= 0 && addressFragments[i-1].equalsIgnoreCase("no") ){
+                    addressNum[0] = addressFragments[i-1];
+                }
+            }
+            else if(containsAInteger( addressFragments[i] )){
+                addressNum[1] = addressFragments[i];
+            }
+        }
+        for (String element : addressFragments) {
+            if(Arrays.asList( addressNum ).contains( element )) formattedAddress[1] += element;
+            else formattedAddress[0] += element + " ";
+        }
+        return formattedAddress;
+    }
+
+    private static boolean isInteger(String str) {
+        return str != null && str.matches("[0-9]*");
+    }
+
+    private static boolean containsAInteger(String str) {
+        String replacedStr = str.replaceAll("(\\D)", "");
+        return replacedStr.length() > 0;
+    }
 }
---
+++ src/main/java/gabriel/io/components/TestCases.java
@@ -0,0 +1,56 @@
+package gabriel.io.components;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestCases {
+    public static void testAll(){
+        ArrayList<String> tests = new ArrayList<>(Arrays.asList(
+            "Miritiba 339",
+            "Babaçu 500",
+            "Cambuí 804B",
+            "Rio Branco 23",
+            "Quirino dos Santos 23 b",
+            "4, Rue de la République",
+            "100 Broadway Av",
+            "Calle Sagasta, 26",
+            "Calle 44 No 1991"
+        ));
+
+        /*Os resultados serão armazenados em uma matriz, sendo:
+        * results[array do resultado][0] = endereço original
+        * results[array do resultado][1] = endereço em um array, separado entre Número e nome
+        * ex:
+        * results[0] -> [Miritiba 339, [Miritiba, 339]]
+        */
+        String[][] results = new String[9][2];
+        int c = 0;
+
+        for (String testCase: tests) {
+            results[c][0] = testCase;
+            results[c][1] = Arrays.toString(AddressValidator.validate(testCase));
+            c++;
+        }
+
+        String[] formattedResults = resultsConversion(results);
+
+        //Exibe uma janela para cada resultado
+        for (String element: formattedResults) {
+            JOptionPane.showMessageDialog(null, element);
+        }
+
+    }
+    //Cria uma formatação para exibir os resultados
+    private static String[] resultsConversion(String[][] test){
+        String[] output = new String[9];
+        for (int i = 0; i < 9; i++) {
+            output[i] = "\nEndereço Original:\n" +
+                    test[i][0] +
+                    "\nEndereço separado (Nome/número):\n" +
+                    test[i][1];
+        }
+
+        return output;
+    }
+}
